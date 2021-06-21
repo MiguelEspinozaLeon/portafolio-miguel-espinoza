5a769807074e29f11801375913360ecdb21fae75
Miguel Espinoza <miguel.espinozaleon@gmail.com>
2021-06-21 13:16:07 -0700

    fin de la seccion 4

---
+++ src/app/interfaces/info-pagina-interface.ts
@@ -0,0 +1,11 @@
+export interface InfoPagina{
+   
+        titulo?: string;
+        email?: string;
+        facebook?: string;
+        twitter?: string;
+        instagram?: string;
+        tumblr?: string;
+        equipotrabajo?: any[];
+    
+}
